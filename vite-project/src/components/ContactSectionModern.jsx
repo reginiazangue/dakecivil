e3f44efa56709b753324565f811440bccde18f2c
KOUODZO ZANGUE REGINIA <reginiazangue84@gmail.com>
2025-12-27 02:04:36 +0100

    Mise à jour frontend React et backend Flask

--- vite-project/src/components/ContactSectionModern.jsx
+++ vite-project/src/components/ContactSectionModern.jsx
@@ -43,7 +43,7 @@ const ContactSectionModern = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/contact", {
+      const response = await fetch("https://dakecivil.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
